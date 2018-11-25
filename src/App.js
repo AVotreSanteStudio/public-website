530558c6c99e5e0040b7b0ba702b22e2755d04f4
Britta Rodenbeck <rodenbeck.britta@gmail.com>
2018-11-25 10:18:56 -0500

    :sparkles: adding subscription form for updates and news

--- src/App.js
+++ src/App.js
@@ -2,6 +2,50 @@ import React, { Component } from 'react';
 import './App.css';
 
 class App extends Component {
+  state = {
+    email: '',
+    error: '',
+    wasSubmitted: false,
+  };
+
+  handleChange = (event) => {
+    const { name, value } = event.target;
+    this.setState({ [name]: value });
+  }
+
+  handleSubmit = () => {
+    const email = this.state.email;
+    fetch('/', {
+      method: 'POST',
+      headers: { 'Content-Type': 'application/x-www-form-urlencoded' },
+      body: this.encode({ 'form-name': 'subscribe', email }),
+    })
+      .then(this.wasSuccessful = true)
+      .catch(error => this.setState({ error: error }));
+    this.wasSubmitted = true;
+  }
+
+  validateForm = (event) => {
+    event.preventDefault();
+    if(this.emailIsValid(this.state.email)) {
+      this.setState({ error: '' });
+      this.handleSubmit();
+    } else {
+      this.setState({ error: 'You must provide a valid email address.' });
+    }
+  }
+
+  emailIsValid = (email) => {
+    const re = /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+    return re.test(String(email).toLowerCase());
+  }
+
+  encode = (data) => {
+    return Object.keys(data)
+      .map(key => `${encodeURIComponent(key)}=${encodeURIComponent(data[key])}`)
+      .join('&');
+  }
+
   render() {
     return (
       <div className="App">
@@ -9,12 +53,19 @@ class App extends Component {
           <h1 className="m-b-20">À Votre Santé Studio</h1>
           <p><span role="img" aria-label="construction emoji">🚧</span></p>
           <p className="m-b-20">Our website is under construction. We will be here soon with our new, <strong>awesome</strong> site.</p>
-          {/* <form>
-            <p className="m-b-15">Subscribe for updates.</p>
-            <label for="email">Email address</label>
-            <input name="email" id="email" placeholder="email address" />
-            <button className="m-l-10">Subscribe</button>
-          </form> */}
+          <div className="form__container">
+            {!this.wasSubmitted &&
+              <form onSubmit={this.validateForm}>
+                <p className="m-b-15">Subscribe for updates.</p>
+                <label htmlFor="email">Email address</label>
+                <input name="email" id="email" type="email" placeholder="email address" value={this.state.email} onChange={this.handleChange} />
+                <button className="m-l-10">Subscribe</button>
+              </form>
+            }
+            {this.wasSubmitted &&
+              <p>Thank you for subscribing. We will keep you up to date with any news!</p>
+            }
+          </div>
         </main>
         <footer>
           <p>&copy; À Votre Santé Studio, LLC 2018 - All rights reserved</p>
